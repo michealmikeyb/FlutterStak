6455fb68a9ded092dc9e15da6af521a8773311d8
Michael Mitchell <michealmikeyb8@gmail.com>
2018-08-30 01:35:26 -0400

    listing update 2

--- lib/contentCard.dart
+++ lib/contentCard.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'listing.dart';
 import 'comments.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:collection';
+import 'tag.dart';
 
 class ContentCard extends StatelessWidget{
   Listing listing;
@@ -32,4 +36,62 @@ class ContentCard extends StatelessWidget{
               );
   }
 
+}
+
+class CardStack extends StatefulWidget{
+  CardStack({Key key }):super(key: key);
+
+  _CardStackState createState() => _CardStackState();
+}
+
+class _CardStackState extends State<CardStack>{
+  Queue<Widget> cardQ;
+  ListingList list;
+  int stackSize;
+  int index;
+
+  void initState(){
+    list = new ListingList();
+    stackSize = 3;
+    index = 0;
+    fillQ();
+  }
+
+  void removeCard(){
+    setState((){
+    cardQ.removeFirst();
+    fillQ();
+    });
+  }
+
+  void fillQ(){
+    while(cardQ.length<=stackSize){
+      Listing newListing = list.getListing();
+      cardQ.add(new Dismissible(
+        key: new Key("$index"),
+        onDismissed:(direction){
+          removeCard();
+          switch (direction) {
+            case DismissDirection.startToEnd:
+            list.like(tag: new SourceName(newListing.tag, newListing.source),id: newListing.id);
+            break;
+            case DismissDirection.endToStart:
+            list.dislike(tag: new SourceName(newListing.tag, newListing.source), id: newListing.id);
+            break;
+            default:
+            break;
+          }
+        } ,
+        child: new ContentCard(listing: newListing),
+      ));
+    }
+    setState(() {
+          
+        });
+  }
+
+ 
+  Widget build(BuildContext context){
+    return new Stack(children: cardQ.toList());
+  }
 }
--- lib/listing.dart
+++ lib/listing.dart
@@ -5,108 +5,252 @@ import 'dart:convert';
 import 'dart:collection';
 import 'tag.dart';
 import 'placeList.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Listing{
+class Listing {
   String _imgLink;
   String _author;
   String _text;
   String _tag;
   String _commentLink;
   String _title;
-  String _sharedBy;
+  String _source;
+  String _id;
 
-  Listing(this._imgLink, this._author, this._text, this._tag, this._commentLink, this._title);
+  Listing(this._imgLink, this._author, this._text, this._tag, this._commentLink,
+      this._title, this._source, this._id);
   get imgLink => _imgLink;
   get author => _author;
   get text => _text;
   get tag => _tag;
   get commentLink => _commentLink;
   get title => _title;
+  get source => _source;
+  get id => _id;
+}
+
+class StakListing extends Listing{
+  String _imgLink;
+  String _author;
+  String _text;
+  String _tag;
+  String _commentLink;
+  String _title;
+  String _source;
+  String _sharedBy;
+  String _id;
+
+  StakListing(this._imgLink, this._author, this._text, this._tag, this._commentLink,
+      this._title, this._source, this._sharedBy, this._id ): super(_imgLink, _author, _text, _tag, _commentLink, _title, _source, _id);
+  get imgLink => _imgLink;
+  get author => _author;
+  get text => _text;
+  get tag => _tag;
+  get commentLink => _commentLink;
+  get title => _title;
+  get source => _source;
+  get sharedBy => _sharedBy;
+  get id => _id;
 
 }
 
-class ListingQueue{
+class ListingQueue {
   Queue<Listing> listingQ;
   String name;
+  String source;
   JsonDecoder decoder;
   String place;
 
-  ListingQueue(this.name, int percent, this.place){
+  ListingQueue(this.name, int percent, this.place, this.source) {
     decoder = new JsonDecoder();
-    
+
     updateTag(percent);
-    
   }
-  void updateTag(int percent)async{
-    if(listingQ.length < percent/2){
-      int numToAdd = percent- listingQ.length;
-      var response;
-       if (place == "not in") {
-        //if its not in gets the first one lin the list
-        response = await http.get(
-            Uri.encodeFull("https://www.reddit.com/r/$name.json?limit=$numToAdd;"),
-            headers: {"Accept": "applications/json"});
-      } else {
-        //else goes to the specified place in the tag
-        response = await http.get(
-            Uri.encodeFull(
-                "https://www.reddit.com/r/$name.json?limit=$numToAdd;after=$place;"),
-            headers: {"Accept": "applications/json"});
-      }
-      var responseJson = decoder.convert(response.body);
-      for(var l in responseJson){
-        Listing listing;
-        if(l['data']!=null){
-          String imgLink = l["data"]["children"][0]["data"]["url"];
-          String author= l["data"]["children"][0]["data"]["author"];
-          String text = l["data"]["children"][0]["data"]["selftext"];
-          String tag = name;
-          String commentLink = l["data"]["children"][0]["data"]["permalink"];
-          String title = l["data"]["children"][0]["data"]["title"];
-          place = l["data"]["after"];
-          listing = new Listing(imgLink, author, text, tag, commentLink, title);
+  void updateTag(int percent) async {
+    if (listingQ.length < percent / 2) {
+      int numToAdd = percent - listingQ.length;
+      if (source == "reddit") {
+        var response;
+        if (place == "not in") {
+          //if its not in gets the first one lin the list
+          response = await http.get(
+              Uri.encodeFull(
+                  "https://www.reddit.com/r/$name.json?limit=$numToAdd;"),
+              headers: {"Accept": "applications/json"});
+        } else {
+          //else goes to the specified place in the tag
+          response = await http.get(
+              Uri.encodeFull(
+                  "https://www.reddit.com/r/$name.json?limit=$numToAdd;after=$place;"),
+              headers: {"Accept": "applications/json"});
         }
-        else{
-          listing = new Listing("https://i.imgur.com/yFW1GdD.png", "error loading", "error loading", "error loading", "error loading", "error loading");
+        var responseJson = decoder.convert(response.body);
+        for (var l in responseJson) {
+          Listing listing;
+          if (l['data'] != null) {
+            String imgLink = l["data"]["children"][0]["data"]["url"];
+            String author = l["data"]["children"][0]["data"]["author"];
+            String text = l["data"]["children"][0]["data"]["selftext"];
+            String tag = name;
+            String commentLink = l["data"]["children"][0]["data"]["permalink"];
+            String title = l["data"]["children"][0]["data"]["title"];
+            place = l["data"]["after"];
+            listing = new Listing(
+                imgLink, author, text, tag, commentLink, title, source, place);
+          } else {
+            listing = new Listing(
+                "https://i.imgur.com/yFW1GdD.png",
+                "error loading",
+                "error loading",
+                "error loading",
+                "error loading",
+                "error loading",
+                "error Loading",
+                "error loading");
+          }
+          listingQ.addFirst(listing);
         }
-        listingQ.addFirst(listing);
+      } else if (source == "stakswipe") {
+        var snapshot = Firestore.instance
+            .collection('listings')
+            .orderBy('adjusted_score')
+            .where('tag', isEqualTo: name)
+            .where('adjusted_score', isLessThan: place)
+            .snapshots();
+        var data = List();
+        await for (var c in snapshot) {
+          data.add(c);
+          if (data.length > numToAdd) break;
+        }
+        for (var l in data) {
+          String title = l['title'];
+          String author = l['author'];
+          String link = l['link'];
+          String text = l['text'];
+          String comments = l['comments'];
+          String id = l.documentId();
+          place = l['adjusted_score'];
+          listingQ.add(new StakListing(
+              link, author, text, name, comments, title, source, "", id));
+        }
       }
     }
   }
 
-  Listing take(){
+  Listing take() {
     return listingQ.removeFirst();
   }
-
 }
 
-class ListingList{
+class ListingList {
   TagList tagList;
   PlaceList placeList;
   List<ListingQueue> list;
+  JsonDecoder decoder;
+  JsonEncoder encoder;
 
-  ListingList(this.tagList, this.placeList){
-    for(Tag t in tagList.allTags){
-      list.add(new ListingQueue(t.name, tagList.getPercent(t.name, t.type).ceil(), placeList.getPlace(t.name, t.type)));
+  ListingList() {
+    decoder = new JsonDecoder();
+    encoder = new JsonEncoder();
+    restore();
+    for (Tag t in tagList.allTags) {
+      list.add(new ListingQueue(
+          t.name,
+          tagList.getPercent(t.name, t.type).ceil(),
+          placeList.getPlace(t.name, t.type),
+          t.type));
     }
   }
 
-  Listing getListing(){
-    SourceName newTag = tagList.getTag();
-    for(ListingQueue q in list){
-      if(q.name == newTag.name){
-        Listing listing = q.take();
-        q.updateTag(tagList.getPercent(newTag.name, newTag.source).ceil());
-        placeList.setPlace(newTag.name, newTag.source, q.place);
+  Listing getListing() {
+    bool isEmpty;
+    do {
+      SourceName newTag = tagList.getTag();
+      for (ListingQueue q in list) {
+        if (q.name == newTag.name) {
+          if (q.listingQ.isEmpty) {
+            isEmpty = true;
+            break;
+          }
+          else
+            isEmpty = false;
+          Listing listing = q.take();
+          q.updateTag(tagList.getPercent(newTag.name, newTag.source).ceil());
+          placeList.setPlace(newTag.name, newTag.source, q.place);
+          break;
+        }
       }
-    }
+    } while (isEmpty);
   }
 
-  void like(SourceName tag){
+  void like({SourceName tag, String id}) {
     tagList.like(tag.name, tag.source);
-  }
-  void dislike(SourceName tag){
-    tagList.dislike(tag.name, tag.source);
+    if (tag.source == "stakuser" || tag.source == "stakswipe")
+      Firestore.instance.runTransaction((transaction) async{
+        var freshSnap = await transaction.get(Firestore.instance.collection('listings').document(id));
+        await transaction.update(freshSnap.reference, {
+         'score': freshSnap.data['score']++
+        });
+      });
+    save();
   }
 
+  void dislike({SourceName tag, String id}) {
+    tagList.dislike(tag.name, tag.source);
+    if (tag.source == "stakuser" || tag.source == "stakswipe")
+      Firestore.instance.runTransaction((transaction) async{
+        var freshSnap = await transaction.get(Firestore.instance.collection('listings').document(id));
+        await transaction.update(freshSnap.reference, {
+         'score': freshSnap.data['score']--,
+        });
+      });
+    save();
+  }
+
+  /**
+   * save the current taglist and placelist
+   * in the sharedpreferences as a json
+   */
+  void save() async {
+    var prefs =
+        await SharedPreferences.getInstance(); //get the shared preferences
+    //convert the taglist and placelist into jsons
+    String taglistJson = encoder.convert(tagList);
+    String placeJson = encoder.convert(placeList);
+    //store those string in the preferences
+    prefs.setString('place', placeJson);
+    prefs.setString('taglist', taglistJson);
+  }
+
+  /**
+   * restore the taglist and placelist from shared preferences 
+   */
+  void restore() async {
+    var prefs =
+        await SharedPreferences.getInstance(); //get the sharedpreferences
+    //get the tagjson and placejson strings, if they aren't there return
+    String tagJson = prefs.getString('taglist') ?? "0";
+    String placeJson = prefs.getString('place') ?? "0";
+    if (tagJson == "0") {
+      tagList = new TagList();
+      placeList = new PlaceList();
+      //setState(() {
+      //cardq = introCard();
+      //});
+      return;
+    }
+    //convert them to a map
+    Map tagmap = decoder.convert(tagJson);
+    Map placemap = decoder.convert(placeJson);
+    //convert that map into the taglist and placelist
+    tagList = new TagList.fromJson(tagmap);
+    placeList = new PlaceList.fromJson(placemap);
+    /**cardq = new Queue();
+    setState(() {
+      cardq.add(newCard());
+      cardq.add(newCard());
+      cardq.add(newCard());
+    });**/
+  }
 }
